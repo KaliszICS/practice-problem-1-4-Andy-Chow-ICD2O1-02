75b09d89a45fc1fb6c20ba035f63aa59a56d4bee
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-09-19 18:41:58 +0000

    finished assig

--- PP1_4.py
+++ PP1_4.py
@@ -1,21 +1,36 @@
-
+'''
+    Title: input and f strings assignment
+    Author: Andy Chow
+    Date Created: Sept 19, 2024
+    Date Last Modified: Sept 19, 2024
+'''
 
 def q1():
+  answer = input("Input a word: ")
+  print(answer)
   #Write Assignment code here
 
 def q2():
+  word = input("Input your first name: ")
+  print(f"Hello {word}")
   #Write Assignment code here
 
 def q3():
+  name1 = input("Input your first name: ")
+  name2 = input("Input your last name: ")
+  print(f"{name2} {name1}")
   #Write Assignment code here
 
 def q4():
+  student1 = input("Input a student: ")
+  student2 = input("Input another student: ")
+  print(f"Your students are {student1} and {student2}")
   #Write Assignment code here
 
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
-q1()
-q2()
-q3()
-q4()
+# q1()
+# q2()
+# q3()
+# q4()
